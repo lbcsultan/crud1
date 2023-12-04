dbf191c3be3f3fc264019723cb16f3a543568a30
Byoungcheon Lee <lbcsultan@gmail.com>
2023-12-04 15:49:45 +0900

    2nd

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,15 @@
 import TopicList from '@/components/TopicList'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect('/signIn')
+  }
+
   return (
     <>
       <h1 className="text-2xl font-bold">WebDev Topics</h1>
